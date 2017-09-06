7e98720935a3ce31cce23f6fdf98157482752e6b
SarnaKhmel <olsarnat@gmail.com>
2017-09-06 23:11:53 +0300

    4th add, nead fix this shit

--- izvp_lab1/izvp_lab1/Class1.cs
+++ izvp_lab1/izvp_lab1/Class1.cs
@@ -3,22 +3,65 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+
 
 namespace izvp_lab1
 {
-    class bikeShop
+     public class bikeShop
     {
+        
+        string[] BikeShop;
+        public static List<string> mark = new List<string>();
+        public static List<string> model = new List<string>();
+        public static List<string> complectation = new List<string>();
+        public static List<int> size = new List<int>();
+        public static List<int> diametr = new List<int>();
+        public static List<double> score = new List<double>();
+        public static List<double> price = new List<double>();
+
+
+
+     
+       public void mainLoad()
+        {
+            mark.Clear(); model.Clear(); complectation.Clear();
+            size.Clear(); diametr.Clear(); score.Clear(); price.Clear();
+            StreamReader reader = new StreamReader("BikeStore.txt", Encoding.Default);
+            BikeShop = reader.ReadToEnd().Split(new char[] { '\n', '|', '\r' }, StringSplitOptions.RemoveEmptyEntries);
+            for (int i = 0; i < BikeShop.Length; i++)
+            {
+                if (i % 2 == 0)
+                    model.Add(BikeShop[i]);
+                else price.Add(double.Parse(BikeShop[i]));
+            }
+            reader.Close();
+        }
+
+        // додаємо товари в комбобокс
+         public void skladLoad()
+        {
+            Form2 frm2 = new Form2 (this);
+            foreach (var tov in orders)
+            {
+                frm2.comboBox1.Items.Add(tov);               
+            }
+            frm2.Show();
+        }
+
+    }
+}
+/*
         private string mark { get; set; }
         private string model { get; set; }
         private string complectation { get; set; }
-       // private int size { get; set; }
+        // private int size { get; set; }
         private int diametp { get; set; }
         private int score { get; set; }
-       // private int price { get; set; }
-        //private int cost { get; set; }
-
-        public bikeShop(string mark, string model, string complectation, int size, int diametr)
-        {
+        // private int price { get; set; }
+        
+        */
+/*
             this.mark = mark;
             this.model = model;
             this.complectation = complectation;
@@ -26,10 +69,10 @@ namespace izvp_lab1
             this.diametp = diametp;
             this.score = score;
            //this.price = price;
-           // this.cost = cost;
-
-
-           string Size ( string sizer)
+           
+    */
+        
+/* string Size ( string sizer)
             {
 
                 if (diametr <= 14)
@@ -73,9 +116,4 @@ namespace izvp_lab1
 
                 if (score > 10 && score < 0) score = 0; 
                 return price;
-            }
-
-        }
-
-    }
-}
+            }*/
